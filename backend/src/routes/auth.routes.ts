2da44f82258dcfa8fa0284d0a76b7e4d9179e31d
imshubhamgiri <skgiri569@gmail.com>
2026-03-20 15:49:44 +0530

    feat:initial implementaion of validation check at user/register route

--- backend/src/routes/auth.routes.ts
+++ backend/src/routes/auth.routes.ts
@@ -1,13 +1,14 @@
 import express from 'express';
 import * as authController from '../controllers/authController';
-const { loginMiddleware } = require('../middleware/auth');
+import { loginMiddleware } from '../middleware/auth';
+import { validateUserRegister } from '../middleware/validation';
 
 const authRoutes = express.Router();
 
 //] authentication routes here
 // User Routes
 authRoutes.post('/user/login',authController.login)
-authRoutes.post('/user/register',authController.register)
+authRoutes.post('/user/register', validateUserRegister, authController.register)
 authRoutes.get('/user/logout',authController.logoutuser)
 // Food Partner Routes
 authRoutes.post('/partner/register',authController.registerFoodPartner)
