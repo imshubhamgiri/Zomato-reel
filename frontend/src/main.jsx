c58fd88014d0e3d1194db6d355aed682eae45398
imshubhamgiri <skgiri569@gmail.com>
2026-03-31 00:04:53 +0530

    wrap app with theme provider

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -3,11 +3,14 @@ import { createRoot } from 'react-dom/client'
 import './index.css'
 import { BrowserRouter } from 'react-router-dom'
 import App from './App.jsx'
+import { ThemeProvider } from './context/ThemeContext.js'
 
 createRoot(document.getElementById('root')).render(
  
     <BrowserRouter>
-      <App />
+    <ThemeProvider>
+     <App />
+     </ThemeProvider>
     </BrowserRouter>
     
 )
