b46b4ba5c90fb73b0efb24c4ed4805a52235643b
imshubhamgiri <skgiri569@gmail.com>
2026-04-11 19:56:37 +0530

    refactor(userModel):extract address schema as a isolated schema definition

--- backend/src/models/userModel.ts
+++ backend/src/models/userModel.ts
@@ -27,27 +27,27 @@ interface User {
     phone?:string;
 }
 
+const AddressSchema = new Schema<UserAddress>({
+    label: { type: String, enum: ['Home', 'Work', 'Other'] },
+    fullName: { type: String, trim: true },
+    phone: { type: String, trim: true },
+    locality: { type: String, trim: true },
+    address: { type: String, trim: true },
+    city: { type: String },
+    state: { type: String },
+    postalCode: { type: String, trim: true },
+    country: { type: String, trim: true },
+    landmark: { type: String, trim: true },
+    alternatePhone: { type: String, trim: true },
+    isDefault: { type: Boolean, default: false }
+});
+
 const UserSchema = new Schema<User>({
     name: { type: String, required: true, trim: true },
     email: { type: String, required: true, unique: true, lowercase: true, trim: true },
     password: { type: String, required: true, select: false } ,
-     gender: { type: String, enum: ['Male', 'Female', 'Other'] },
-    address: [
-        {
-            label: { type: String, enum: ['Home', 'Work', 'Other'] },
-            fullName: { type: String, trim: true },
-            phone: { type: String, trim: true },
-            locality: { type: String, trim: true },
-            address: { type: String, trim: true },
-            city: { type: String },
-            state: { type: String },
-            postalCode: { type: String, trim: true },
-            country: { type: String, trim: true },
-            landmark: { type: String, trim: true },
-            alternatePhone: { type: String, trim: true },
-            isDefault: { type: Boolean, default: false }
-        }
-    ],
+    gender: { type: String, enum: ['Male', 'Female', 'Other'] },
+    address: [AddressSchema],
     phone: { type: String }
 },{
     timestamps:true
